41664df46d2992cd5820d207cd78dde5966539fe
AdrianM <30712077+amichot@users.noreply.github.com>
2019-05-27 20:48:25 -0500

    wrote tests for api requests

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
-const app = require("./app");
+//dependencies
+const knex = require('knex');
+//components
+const app = require('./app');
+const {PORT, DB_URL} = require('./config');
 
-const { PORT } = require("./config");
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+});
+
+app.set('db', db);
 
 app.listen(PORT, () => {
-	console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
